727fb5cfd3b09c5d44e0d3f1758e3aaf33f13f39
Ray Jackson <roguej2@gmail.com>
2019-07-21 22:32:44 -0400

    script to paint textures on a terrain

---
+++ paintTerrain.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class paintTerrain : MonoBehaviour
+{
+    [System.Serializable]
+    public class SplatHeights
+    {
+        public int textureIndex;
+        public float startingHeight;
+    }
+
+    public SplatHeights[] colorHeights;
+    TerrainData tdata;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        tdata = Terrain.activeTerrain.terrainData;
+        int yMax = tdata.alphamapHeight;
+        int xMax = tdata.alphamapWidth;
+
+        float[,,] splatmapData = new float[xMax,
+                                            yMax,
+                                            tdata.alphamapLayers];
+
+        for (int y = 0; y < yMax; y++)
+        {
+            for (int x = 0; x < xMax; x++)
+            {
+                float terrainHeight = tdata.GetHeight(y, x);
+                float[] splat = new float[colorHeights.Length];
+
+                for (int i = 0; i < colorHeights.Length; i++)
+                {
+                    if (terrainHeight >= colorHeights[i].startingHeight)
+                    {
+                        splat[i] = 1;
+                    }
+                }
+                for (int j = 0; j < colorHeights.Length; j++)
+                {
+                    splatmapData[x, y, j] = splat[j];
+                }
+            }
+        }
+
+        tdata.SetAlphamaps(0, 0, splatmapData);
+    }
+
+
+}
